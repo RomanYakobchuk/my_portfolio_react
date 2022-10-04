c4d183cc0b7429120f43cb1d61566d7428a8f7e3
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-10-04 17:16:09 +0300

    main1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {useEffect, useState} from "react";
 
 import logo from '../../assets/img/logo.svg'
-import navIcon1 from '../../assets/img/nav-icon1.svg'
 import navIcon2 from '../../assets/img/nav-icon2.svg'
 import navIcon3 from '../../assets/img/nav-icon3.svg'
 
